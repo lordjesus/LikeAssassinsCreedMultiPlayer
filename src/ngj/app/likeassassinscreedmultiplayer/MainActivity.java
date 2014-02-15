938c1b7a824e78bd4b31c7ffeeb2b6cfea5e3c3a
cristi16 <cristy_veep@yahoo.com>
2014-02-15 16:23:33 +0100

    sign-in button

--- src/ngj/app/likeassassinscreedmultiplayer/MainActivity.java
+++ src/ngj/app/likeassassinscreedmultiplayer/MainActivity.java
@@ -3,13 +3,23 @@ package ngj.app.likeassassinscreedmultiplayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.api.*;
+import com.google.example.games.basegameutils.BaseGameActivity;
+
+
+public class MainActivity extends BaseGameActivity 
+						implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+  
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this); 
     }
 
 
@@ -19,5 +29,40 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onSignInFailed() {
+		// Sign in has failed. So show the user the sign-in button.
+	    findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+	    findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+	}
+
+
+	@Override
+	public void onSignInSucceeded() {
+		 // show sign-out button, hide the sign-in button
+	    findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+	    findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+
+	    // (your code here: update UI, enable functionality that depends on sign in, etc)
+	}
+
+
+	@Override
+	public void onClick(View view) {
+	    if (view.getId() == R.id.sign_in_button) {
+	        // start the asynchronous sign in flow
+	        beginUserInitiatedSignIn();
+	    }
+	    else if (view.getId() == R.id.sign_out_button) {
+	        // sign out.
+	        signOut();
+
+	        // show sign-in button, hide the sign-out button
+	        findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+	        findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+	    }
+	}
     
 }
